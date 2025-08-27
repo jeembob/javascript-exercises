8d4ec6033d90a742027facfcec01e1fc0b2245be
jim <jimwlee@gmail.com>
2025-08-27 16:28:15 +0800

    Add 06 and 07 js string exercise solutions

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  let output = ''
+  if (times < 0) {
+    return "ERROR";
+  } else {
+    for (i = times; i > 0; i--) {
+      output += word;
+    }
+  }
+  return output;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const length = string.length
+    let newString = ''
+    for (i = 0; i < length ; i++) { //length five go 5 times
+        newString += string[length - 1 - i] //take the position minus i, minus the correction for position 0 -4
+    }
+    return newString;
 };
 
 // Do not edit below this line
