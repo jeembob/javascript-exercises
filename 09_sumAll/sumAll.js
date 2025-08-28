faf7730c067f63af4514f409609b2b07ab6c6fde
jim <jimwlee@gmail.com>
2025-08-28 14:39:16 +0800

    Add 09 Sum all Solution
    Note to self: messed up a lot when trying to typeof === string
    because you were testing them post evalution where "90" was already
    being coerced into a number

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  const startNum = Math.min(a, b);
+  const endNum = Math.max(a, b);
 
+  if (
+    startNum < 0 ||
+    endNum < 0 ||
+    !Number.isInteger(startNum) ||
+    !Number.isInteger(endNum) ||
+    typeof a !== "number" ||
+    typeof b !== "number"
+  ) {
+    return "ERROR";
+  }
+
+  for (i = startNum; i <= endNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
