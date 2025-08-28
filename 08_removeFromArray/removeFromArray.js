a1a33b341fb96cb6ff3bdae55d0f1aab3ac889ec
jim <jimwlee@gmail.com>
2025-08-28 14:05:16 +0800

    Add 08 removeFromArray solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removal) {
+    const filtered = array.filter((element) => !removal.includes(element))
+    array.length = 0;
+    array.push(filtered);
+    console.log(filtered);
+    return filtered;
 };
+
+//for each element, check if needs to be removed
+//dump it into a new array
 
 // Do not edit below this line
 module.exports = removeFromArray;
