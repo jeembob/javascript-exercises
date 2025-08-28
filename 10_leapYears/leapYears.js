19d26ac06aba5efbfa7ea39484231bb9300f5cf7
jim <jimwlee@gmail.com>
2025-08-28 14:54:10 +0800

    Add 10_leapYears solution

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  //divisible by four === leap year
 
+  //if % 100 = 0 not leap years unless % 400 ===0
+  if (year % 100 === 0 && year % 400 !== 0) {
+    return false;
+  } else {
+    return year % 4 === 0;
+  }
 };
 
 // Do not edit below this line
